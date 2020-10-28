2e4adced94277693118a223ce66982e4bebee93f
ted <tedmarov@gmail.com>
2020-10-28 14:37:52 -0500

    Eatery

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 import {getParks} from "./parks/ParkProvider.js"
 import {parkSelect} from "./parks/ParkSelect.js"
-import { getEateries } from "./eateries/EateryProvider.js"
-import { eaterySelect } from "./eateries/EaterySelect.js"
 import { getAttractions } from "./attractions/AttractionProvider.js"
 import { attractionSelect } from "./attractions/AttractionSelect.js"
+import { getEateries } from "./eateries/EateryProvider.js"
+import { eaterySelect } from "./eateries/EaterySelect.js"
 
 parkSelect()
 getParks()
