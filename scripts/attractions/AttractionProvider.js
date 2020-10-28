99d478ec7ac0763c5ed574e253f7ac135456515c
Travis Milner <travismilner1@gmail.com>
2020-10-28 14:21:10 -0500

    added the drop down box for attractions and rendered the attractions to the dom

---
+++ scripts/attractions/AttractionProvider.js
@@ -0,0 +1,18 @@
+
+
+let attractions = []
+
+export const useAttractions = () => {
+    return attractions.slice()
+}
+
+export const getAttractions = () => {
+    return fetch("http://holidayroad.nss.team/bizarreries")
+    .then(response => response.json())
+    .then(
+        parsedAttractions => {
+            attractions = parsedAttractions
+            console.log(attractions)
+        }
+    )
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 import {getParks} from "./parks/ParkProvider.js"
 import {parkSelect} from "./parks/ParkSelect.js"
+import { getAttractions } from "./attractions/AttractionProvider.js"
+import { attractionSelect } from "./attractions/AttractionSelect.js"
 
 parkSelect()
 getParks()
+
+getAttractions()
+attractionSelect()
