ae9b5a0a662178f9c23d1c511c62ac8c530eca39
David Bing <davidb.devops@gmail.com>
2020-10-29 14:03:27 -0500

    update module: main.js import/invoke getWeather/WeatherSelect

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,15 @@ import { attractionSelect } from "./attractions/AttractionSelect.js"
 import { eaterySelect } from "./eateries/EaterySelect.js"
 import { eateries } from "./eateries/SingleEateryRender.js"
 import "./attractions/SingleAttractionRender.js"
+import { getWeather } from "./weather/WeatherProvider.js"
+import { weatherSelect } from "./weather/WeatherSelect.js"
 
 
 parkSelect()
 getParks()
 getAttractions()
+getWeather()
+weatherSelect()
 attractionSelect()
 eaterySelect()
 eateries()
