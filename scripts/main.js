6be72b044ceefc53ed902e7628d1712ef10d09b0
ted <tedmarov@gmail.com>
2020-10-30 09:27:10 -0500

    Detail button works

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ import { attractionSelect } from "./attractions/AttractionSelect.js"
 import { eaterySelect } from "./eateries/EaterySelect.js"
 import { eateries } from "./eateries/SingleEateryRender.js"
 import "./attractions/SingleAttractionRender.js"
+import "./eateries/EateryDetail.js"
 
 parkSelect()
 getParks()
