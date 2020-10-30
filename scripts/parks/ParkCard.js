9f0816602541a9e95be573cdd8b79a810d90fc96
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-30 09:53:50 -0500

    almost got the details button coded to display the details of each park

--- scripts/parks/ParkCard.js
+++ scripts/parks/ParkCard.js
@@ -1,8 +1,27 @@
+const eventHub = document.querySelector(".container")
+
+
 export const Park = (parkObj) => {
     return `
     <div class="park">
-        <p><b>${parkObj.fullName}<button id="details-Button">Details</button></b></p>
+        <p><b>${parkObj.fullName}<button id="details-Button--${parkObj.id}">Details</button></b></p>
         <p>${parkObj.addresses[0].city}</p>
     </div>
     `
 }
+
+
+eventHub.addEventListener("click", (clickEvent) => {
+    const [nameOfId, parkId] = clickEvent.target.id.split("--")
+
+        if(clickEvent.target.id.startsWith("details-Button--")) {
+            console.log("button was clicked:", nameOfId, parkId)
+        const detailsButtonClicked = new CustomEvent("detailsClicked",{
+            detail: {
+                parkId: parkId
+            }
+        })
+
+        eventHub.dispatchEvent(detailsButtonClicked)
+    }
+})
---
+++ scripts/parks/ParkDetailList.js
@@ -0,0 +1,1 @@
+import {useNationalParks} from "./ParkProvider.js"
