f640d2889e0a40535bb8a107ba7f26324984c134
ted <tedmarov@gmail.com>
2020-11-02 09:16:40 -0600

    Updated CSS, round corners

--- scripts/eateries/EateryDetail.js
+++ scripts/eateries/EateryDetail.js
@@ -19,8 +19,12 @@ const render = (eateryObject) => {
 const detailTarget = document.querySelector(`#eatery-${eateryObject.id}`)
     
     detailTarget.innerHTML += `
-        <dd>${eateryObject.city}</dd>
-        <dd>${eateryObject.state}</dd>
-        <dd>${eateryObject.description}</dd>
+    <ul>
+        <dd><b>Wheelchair Accessible:</b> ${eateryObject.ameneties.wheelchairAccessible}</dd>
+        <dd><b>Wi-Fi:</b> ${eateryObject.ameneties.wifi}</dd>
+        <dd><b>City:</b> ${eateryObject.city}</dd>
+        <dd><b>State:</b> ${eateryObject.state}</dd>
+        <dd><b>Description:</b> ${eateryObject.description}</dd>
+    </ul>    
         `
 }
